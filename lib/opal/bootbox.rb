2d608228ce45e60cf28876be128d8d4e785eec90
balmoral <colgunn@me.com>
2015-12-30 18:17:32 +1100

    initial commit

--- lib/opal/bootbox.rb
+++ lib/opal/bootbox.rb
@@ -24,25 +24,39 @@ module Opal
 
     # Creates a confirm window.
     # The result passed to given block is true or false.
+    # If no block is given then true or false or nil confirmation is returned.
     def confirm(*args, &block)
-      bootbox_call(__method__, *args, &block)
+      if block
+        bootbox_call(__method__, *args, &block)
+      else
+        bootbox_call(__method__, *args) do |result|
+          result
+        end
+      end
     end
 
     # Creates a prompt window.
     # The result passed to given block is a String or nil.
+    # If no block is given then the entered value or nil is returned.
     def prompt(*args, &block)
-      bootbox_call(__method__, *args, &block)
+      if block
+        bootbox_call(__method__, *args, &block)
+      else
+        bootbox_call(__method__, *args) do |value|
+          value
+        end
     end
 
   end
 end
 
-else # opal
+else # RUBY_ENGINE
 
   require 'opal'
   require 'opal/bootbox/version'
   Opal.append_path File.expand_path('../..', __FILE__).untaint
-end
+
+end # RUBY_ENGINE
 
 $bootbox = Opal::Bootbox
 
