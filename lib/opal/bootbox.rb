702f097fafa6f0232b0287af82073640da7d331e
balmoral <colgunn@me.com>
2015-12-31 14:11:46 +1100

    initial commit

--- lib/opal/bootbox.rb
+++ lib/opal/bootbox.rb
@@ -11,30 +11,36 @@ module Opal
   module Bootbox
     module_function
 
-    def bootbox_call(method, arg, &block)
-      Native.call(`bootbox`, method, arg.to_n, &block)
+    def bootbox_call(method, *args, &block)
+      arg = args.first
+      if arg.is_a?(Hash)
+        arg[:callback] = block
+        Native.call(`bootbox`, method, arg.to_n)
+      else
+        Native.call(`bootbox`, method, arg.to_n, &block)
+      end
     end
 
     # Creates an alert window.
     # The given block is optional.
     # Method executes asynchronously.
     # No result is passed to the given block.
-    def alert(arg, &block)
-      bootbox_call(__method__, arg, &block)
+    def alert(*args, &block)
+      bootbox_call(__method__, *args, &block)
     end
 
     # Creates a confirm window.
     # Method executes asynchronously.
     # The result passed to given block is true or false.
-    def confirm(arg, &block)
-      bootbox_call(__method__, arg, &block)
+    def confirm(*args, &block)
+      bootbox_call(__method__, *args, &block)
     end
 
     # Creates a prompt window.
     # Method executes asynchronously.
     # The result passed to given block is a String or nil.
-    def prompt(arg, &block)
-      bootbox_call(__method__, arg, &block)
+    def prompt(*args, &block)
+      bootbox_call(__method__, *args, &block)
     end
 
   end
